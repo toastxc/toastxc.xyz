6ea11c560e9c891dd547bcd2ae81040f964995e8
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-02-11 13:01:15 +0800

    uploaded run script

---
+++ gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+adduce -c config/404 -n 404.html -o output
+adduce -c config/index -n index.html -o output
+adduce -c config/minecraft -n minecraft.html -o output
+adduce -c config/projects -n projects.html -o output
