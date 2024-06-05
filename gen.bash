160073da031cd29ed62580717ffe79083d940fc3
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-06-05 13:02:44 +0800

    removed warnings

--- gen.bash
+++ gen.bash
@@ -2,6 +2,9 @@
 s="config"
 t="target"
 
+
+mkdir -p $t
+
 # shellcheck disable=SC2045
 for item in $(ls -f config*)
 do
@@ -10,7 +13,7 @@ if [ "$item" != "global" ] && [ "$char" != "_" ] && [ "$char" != "." ]; then
     echo "$item"
 
   if [ "$item" != "index" ]; then
-  mkdir "$t/$item"
+  mkdir -p "$t/$item"
       adduce -c "$s/$item" -n "index.html" -o "$t/$item"
   else
     adduce -c "$s/$item" -n "$item.html" -o $t
