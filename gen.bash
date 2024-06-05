6e110c660bc06dd6aa0b6ab4560a72f777cc7f0b
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-06-05 12:59:21 +0800

    changed HTML files to directories.

--- gen.bash
+++ gen.bash
@@ -8,7 +8,13 @@ do
   char=${item:0:1}
 if [ "$item" != "global" ] && [ "$char" != "_" ] && [ "$char" != "." ]; then
     echo "$item"
+
+  if [ "$item" != "index" ]; then
+  mkdir "$t/$item"
+      adduce -c "$s/$item" -n "index.html" -o "$t/$item"
+  else
     adduce -c "$s/$item" -n "$item.html" -o $t
+    fi
   fi
 done
 
